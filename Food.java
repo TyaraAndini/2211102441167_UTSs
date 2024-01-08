9d4d553ac52dc30e850f9a4ae38dafbff1dc350f
TyaraAndini <145677710+TyaraAndini@users.noreply.github.com>
2024-01-08 17:22:12 +0800

    Delete Food.java

--- Food.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;
-
-public class Food extends Actor
-{
-    public Food()
-    {
-        setImage("food.png"); // Ganti dengan nama file gambar makanan yang Anda inginkan
-    }
-}
