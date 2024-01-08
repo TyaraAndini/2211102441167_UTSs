4525b32cf4b533500709111900c84eec37025bb7
TyaraAndini <145677710+TyaraAndini@users.noreply.github.com>
2024-01-08 17:24:09 +0800

    Delete SnakeSegment.java

--- SnakeSegment.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;
-
-public class SnakeSegment extends Actor
-{
-    public SnakeSegment(Snake snake)
-    {
-        setImage("snake_segment.png"); // Gambar segmen ular
-    }
-}
